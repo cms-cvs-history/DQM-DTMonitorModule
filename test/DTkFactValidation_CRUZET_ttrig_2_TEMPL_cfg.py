7927aabc825ab65899b99e3b93f79691f748cfad
cmsbuild <>
2009-06-10 10:36:03 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- DTMonitorModule-V03-09-00-10'.
    Sprout from master 2009-06-10 10:36:02 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Add blocked channel summary'
    Cherrypick from master 2008-11-24 09:29:02 UTC giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch> 'added read File card':
        src/DTAlbertoBenvenutiTask.h
        test/DTkFactValidation_CRUZET_ttrig_1_TEMPL_cfg.py
        test/DTkFactValidation_CRUZET_ttrig_2_TEMPL_cfg.py
    Cherrypick from master 2008-07-24 12:56:37 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Move access to DB to beginRun':
        src/DTAlbertoBenvenutiTask.cc
    Delete:
        test/DTkFactValidation_1_TEMPL_cfg.py
        test/DTkFactValidation_2_DQM_TEMPL_cfg.py
        test/DTkFactValidation_2_TEMPL_cfg.py
        test/crab_Valid_TEMPL.cfg

---
+++ test/DTkFactValidation_CRUZET_ttrig_2_TEMPL_cfg.py
@@ -0,0 +1,111 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+process.load("Geometry.DTGeometry.dtGeometry_cfi")
+
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cff")
+
+process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
+
+process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(100),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    interval = cms.uint64(90)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.eventInfoProvider = cms.EDFilter("EventCoordinatesSource",
+    eventInfoFolder = cms.untracked.string('EventInfo/')
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('resolutionTest_step1', 
+        'resolutionTest_step2', 
+        'resolutionTest_step3'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR'),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        FwkJob = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        resolution = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000)
+        ),
+        noLineBreaks = cms.untracked.bool(True)
+    ),
+    categories = cms.untracked.vstring('resolution'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.qTester = cms.EDFilter("QualityTester",
+    prescaleFactor = cms.untracked.int32(1),
+    qtList = cms.untracked.FileInPath('DQM/DTMonitorClient/test/QualityTests_ttrig.xml')
+)
+
+process.resolutionTest_step1 = cms.EDFilter("DTResolutionTest",
+    OutputMEsInRootFile = cms.bool(False),
+    runningStandalone = cms.untracked.bool(True),
+    resDistributionTestName = cms.untracked.string('ResidualsDistributionGaussianTest'),
+    histoTag2D = cms.untracked.string('hResDistVsDist_STEP1'),
+    calibModule = cms.untracked.bool(True),
+    meanTestName = cms.untracked.string('ResidualsMeanInRange'),
+    STEP = cms.untracked.string('STEP1'),
+    folderRoot = cms.untracked.string(''),
+    sigmaTestName = cms.untracked.string('ResidualsSigmaInRange'),
+    debug = cms.untracked.bool(False),
+    diagnosticPrescale = cms.untracked.int32(1),
+    histoTag = cms.untracked.string('hResDist_STEP1'),
+    inputFile = cms.untracked.string('/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/CRUZET/ttrig/DTkFactValidation_RUNNUMBERTEMPLATE.root')
+)
+
+process.resolutionTest_step2 = cms.EDFilter("DTResolutionTest",
+    OutputMEsInRootFile = cms.bool(False),
+    runningStandalone = cms.untracked.bool(True),
+    resDistributionTestName = cms.untracked.string('ResidualsDistributionGaussianTest'),
+    histoTag2D = cms.untracked.string('hResDistVsDist_STEP2'),
+    calibModule = cms.untracked.bool(True),
+    meanTestName = cms.untracked.string('ResidualsMeanInRange'),
+    STEP = cms.untracked.string('STEP2'),
+    folderRoot = cms.untracked.string(''),
+    sigmaTestName = cms.untracked.string('ResidualsSigmaInRange'),
+    debug = cms.untracked.bool(False),
+    diagnosticPrescale = cms.untracked.int32(1),
+    histoTag = cms.untracked.string('hResDist_STEP2'),
+    inputFile = cms.untracked.string('/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/CRUZET/ttrig/DTkFactValidation_RUNNUMBERTEMPLATE.root')
+)
+
+process.resolutionTest_step3 = cms.EDFilter("DTResolutionTest",
+    OutputMEsInRootFile = cms.bool(True),
+    runningStandalone = cms.untracked.bool(True),
+    resDistributionTestName = cms.untracked.string('ResidualsDistributionGaussianTest'),
+    histoTag2D = cms.untracked.string('hResDistVsDist_STEP3'),
+    calibModule = cms.untracked.bool(True),
+    OutputFileName = cms.string('/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/CRUZET/ttrig/SummaryResiduals_RUNNUMBERTEMPLATE.root'),
+    meanTestName = cms.untracked.string('ResidualsMeanInRange'),
+    STEP = cms.untracked.string('STEP3'),
+    folderRoot = cms.untracked.string(''),
+    sigmaTestName = cms.untracked.string('ResidualsSigmaInRange'),
+    debug = cms.untracked.bool(False),
+    diagnosticPrescale = cms.untracked.int32(1),
+    histoTag = cms.untracked.string('hResDist_STEP3'),
+    inputFile = cms.untracked.string('/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/CRUZET/ttrig/DTkFactValidation_RUNNUMBERTEMPLATE.root')
+)
+
+process.secondStep = cms.Sequence(process.resolutionTest_step1*process.resolutionTest_step2*process.resolutionTest_step3*process.qTester)
+process.p = cms.Path(process.secondStep)
+process.DQM.collectorHost = ''
+
+
